63fea3feb3e2e4f9a260a4c7f65b7d8db0e8e979
pranavrai070 <pranavrai070@gmail.com>
2024-07-15 14:50:10 +0530

    remove product detail token middleware

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,7 +4,7 @@ const productController = require('../controllers/productController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/',productController.getProducts);
-router.post('/product',authMiddleware.protect,productController.getProduct);
+router.post('/product',productController.getProduct);
 router.post('/', authMiddleware.protect, productController.createProduct);
 
 module.exports = router;
